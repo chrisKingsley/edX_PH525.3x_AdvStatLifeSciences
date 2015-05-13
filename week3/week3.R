df4eb6353ed9cf29574c03f229e9f45906eefce5
chrisKingsley <kingsley.chris@gmail.com>
2015-05-12 22:58:19 -0700

    updates to week3

--- week3/week3.R
+++ week3/week3.R
@@ -1,12 +1,18 @@
 # code for HarvardX: PH525.3x Advanced Statistics for the Life Sciences - week 3
 
 library(dplyr)
+library(Biobase)
+library(qvalue)
+library(genefilter)
 
-library(dagdata)
-data(admissions)
 
 # load admissions table
 load('admissions.rda')
+
+# load gene expression data
+load("GSE5859.rda")
+geneExpression <- exprs(e)
+sampleInfo <- pData(e)
 
 
 # Question 3.1.1
@@ -55,3 +61,77 @@ cor(admissions$Number[admissions$Gender==0], admByMajor$fracAccepted)
 
 
 # Question 3.2.1
+sampleInfo$year <- format(sampleInfo$date,"%y")
+ethPerYear <- sampleInfo %>%
+    group_by(year) %>%
+    summarise(numGroups=length(unique(ethnicity)))
+sum(ethPerYear$numGroups > 1)
+
+
+# Question 3.2.2
+sampleInfo$month.year = format(sampleInfo$date,"%m%y")
+ethPerMonthYear <- sampleInfo %>%
+    group_by(month.year) %>%
+    summarise(numGroups=length(unique(ethnicity)))
+mean(ethPerMonthYear$numGroups > 1)
+
+
+# Question 3.2.3
+idx1 <- which(as.character(sampleInfo$ethnicity)=="CEU" & sampleInfo$year=="02")
+idx2 <- which(as.character(sampleInfo$ethnicity)=="CEU" & sampleInfo$year=="03")
+outcome <- rep(NA, nrow(sampleInfo))
+outcome[idx1] <- 1
+outcome[idx2] <- 2
+pvals <- rowttests(geneExpression, as.factor(outcome))$p.value
+qvals <- qvalue(pvals)
+sum(qvals$qvalues < 0.05)
+qvals$pi0
+
+
+# Question 3.2.4
+idx1 <- which(as.character(sampleInfo$ethnicity)=="CEU" & sampleInfo$year=="03")
+idx2 <- which(as.character(sampleInfo$ethnicity)=="CEU" & sampleInfo$year=="04")
+outcome <- rep(NA, nrow(sampleInfo))
+outcome[idx1] <- 1
+outcome[idx2] <- 2
+pvals <- rowttests(geneExpression, as.factor(outcome))$p.value
+qvals <- qvalue(pvals)
+sum(qvals$qvalues < 0.05)
+
+
+# Question 3.2.5
+idx1 <- which(as.character(sampleInfo$ethnicity)=="CEU")
+idx2 <- which(as.character(sampleInfo$ethnicity)=="ASN")
+outcome <- rep(NA, nrow(sampleInfo))
+outcome[idx1] <- 1
+outcome[idx2] <- 2
+pvals <- rowttests(geneExpression, as.factor(outcome))$p.value
+qvals <- qvalue(pvals)
+sum(qvals$qvalues < 0.05)
+
+
+# Question 3.2.6
+idx1 <- which(as.character(sampleInfo$ethnicity)=="CEU" & sampleInfo$year=="05")
+idx2 <- which(as.character(sampleInfo$ethnicity)=="ASN" & sampleInfo$year=="05")
+outcome <- rep(NA, nrow(sampleInfo))
+outcome[idx1] <- 1
+outcome[idx2] <- 2
+pvals <- rowttests(geneExpression, as.factor(outcome))$p.value
+qvals <- qvalue(pvals)
+sum(qvals$qvalues < 0.05)
+
+
+# Question 3.2.7
+set.seed(3)
+idx1 <- which(as.character(sampleInfo$ethnicity)=="CEU" & sampleInfo$year=="02")
+idx1 <- sample(idx1, 3)
+idx2 <- which(as.character(sampleInfo$ethnicity)=="ASN" & sampleInfo$year=="05")
+outcome <- rep(NA, nrow(sampleInfo))
+outcome[idx1] <- 1
+outcome[idx2] <- 2
+pvals <- rowttests(geneExpression, as.factor(outcome))$p.value
+qvals <- qvalue(pvals)
+sum(qvals$qvalues < 0.05)
+
+
+# Question 3.3.1
