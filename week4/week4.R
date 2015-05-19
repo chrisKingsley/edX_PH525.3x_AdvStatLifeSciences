1d11c769433ab1357a8fbcb3ce7e9fc1384f6145
chrisKingsley <kingsley.chris@gmail.com>
2015-05-18 22:03:39 -0700

    updates to week4

--- week4/week4.R
+++ week4/week4.R
@@ -1,5 +1,18 @@
 # code for HarvardX: PH525.3x Advanced Statistics for the Life Sciences - week 4
 
+library(genefilter)
+library(limma)
+library(R.utils)
+
+library(tissuesGeneExpression)
+data("tissuesGeneExpression")
+
+library(dagdata)
+data(hcmv)
+
+library(rafalib)
+mypar2()
+
 # Question 4.1.1
 dbinom(2, 4, 0.49)
 
@@ -62,3 +75,67 @@ dbinom(2,N,p)
 dpois(2, N*p)
 
 1 - dpois(0, N*p) - dpois(1, N*p)
+
+
+# Question 4.2.1
+breaks <- seq(0, 4000*round(max(locations)/4000), 4000)
+tmp <- cut(locations, breaks)
+counts <- as.numeric(table(tmp))
+hist(counts)
+
+logprobs <- dpois(counts, 4, log=TRUE)
+loglikelihood <- sum(logprobs)
+loglikelihood
+
+logLikPois <- function(lambda, counts) {
+    logProbs <- dpois(counts, lambda, log=TRUE)
+    sum(logProbs)
+}
+lambdas <- seq(0,15,len=300)
+logLikelihoods <- sapply(lambdas, logLikPois, counts)
+plot(lambdas, logLikelihoods)
+idx <- which(logLikelihoods==max(logLikelihoods))
+printf("maxLikelihood:%f   lambda:%f", logLikelihoods[idx], lambdas[idx])
+
+
+# Question 4.2.2
+binLocation <- (breaks[-1]+breaks[-length(breaks)])/2
+plot(binLocation,counts,type="l",xlab=)
+idx <- which(counts==max(counts))
+printf("binLocation:%s counts:%s", binLocation[idx], counts[idx])
+
+
+# Question 4.2.3
+lambda <- mean(counts[ - which.max(counts) ])
+ppois(13, lambda, lower.tail=F)
+
+
+#  Question 4.2.4 
+0.05/length(binLocation)
+
+
+#  Question 4.2.5
+ps <- (seq(along=counts) - 0.5)/length(counts)
+lambda <- mean( counts[ -which.max(counts)])
+poisq <- qpois(ps,lambda)
+qqplot(poisq,counts)
+abline(0, 1)
+
+
+# Question 4.3.1
+y <- e[,which(tissue=="endometrium")]
+vars <- apply(y, 1, var, na.rm=T)
+qqnorm(vars)
+abline(0, 1)
+qqnorm(sqrt(vars))
+abline(0, 1)
+
+
+# Question 4.3.2
+f_fit <- fitFDist(vars, 14)
+ps <- (seq(along=vars) - 0.5)/length(vars)
+f_quantiles <- qf(ps, f_fit$scale, f_fit$df2)
+qqplot(sqrt(vars), f_quantiles)
+abline(0, 1)
+qqplot(sqrt(vars[vars<quantile(vars, 0.95)]), f_quantiles, ylim=c(0,10))
+abline(0, 1)
